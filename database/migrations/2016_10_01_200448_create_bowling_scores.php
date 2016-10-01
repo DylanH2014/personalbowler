d50096a419da4e4fe591c5a91f978e5d516673ea
DylanH <dharrington2012@yahoo.com>
2016-10-01 16:06:57 -0400

    first commit

---
+++ database/migrations/2016_10_01_200448_create_bowling_scores.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBowlingScores extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bowling_scores', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user');
+            $table->string('score');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('bowling_scores');
+    }
+}
